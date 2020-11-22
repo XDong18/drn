964053dafe50c0a99644208429f3a9401bc2196a
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-22 23:30:31 +0800

    cc

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,4 +2,4 @@ export PYTHONPATH="${PYTHONPATH}:/shared/xudongliu/code/f_server/drn/lib"
 export CUDA_VISIBLE_DEVICES=2,3,4,5
 python3 segment.py train -d /shared/xudongliu/bdd100k/seg/seg -c 19 -s 768 --arch drn_d_38 \
 --random-scale 2 --random-rotate 0 --batch-size 8 --epochs 500 \
---lr 0.01 --momentum 0.9 -j 16 --lr-mode poly --save_path out/drn_d_38_up_768x768_500e --bn-sync
+--lr 0.01 --momentum 0.9 -j 16 --lr-mode poly --save_path out/drn_d_38_up_768x768_500e
