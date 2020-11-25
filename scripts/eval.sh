62fcace7e091a349572e151ad88309fa70653916
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-25 10:39:09 +0800

    cc

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -3,4 +3,4 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 python3 segment.py train -d /shared/xudongliu/bdd100k/seg/seg -c 19 -s 768 --arch drn_d_38 \
 --random-scale 2 --random-rotate 0 --batch-size 8 --epochs 500 \
 --lr 0.01 --momentum 0.9 -j 16 --lr-mode poly --save_path out/drn_d_38_up_768x768_500e \
--e --resume out/drn_d_38_up_768x768_500e/checkpoint_latest.pth.tar
+-e --resume out/drn_d_38_up_768x768_500e/model_best.pth.tar
